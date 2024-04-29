e33c55997e33dadcb1ee35cb89cd240c448671e0
Carlos Moreira <cmoreira@misfirm.com>
2024-04-29 09:24:15 -0400

    protocol change from TCP to Tcp

--- locals.tf
+++ locals.tf
@@ -14,7 +14,7 @@ locals {
       priority                   = 100
       direction                  = "Inbound"
       access                     = "Allow"
-      protocol                   = "TCP"
+      protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "22"
       source_address_prefix      = "VirtualNetwork"
@@ -25,7 +25,7 @@ locals {
       priority                   = 200
       direction                  = "Inbound"
       access                     = "Allow"
-      protocol                   = "TCP"
+      protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "80"
       source_address_prefix      = "*"
@@ -36,7 +36,7 @@ locals {
       priority                   = 300
       direction                  = "Inbound"
       access                     = "Allow"
-      protocol                   = "TCP"
+      protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "443"
       source_address_prefix      = "*"
@@ -47,7 +47,7 @@ locals {
       priority                   = 400
       direction                  = "Inbound"
       access                     = "Allow"
-      protocol                   = "TCP"
+      protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "25"
       source_address_prefix      = "*"
