85eee5e9d1525953179badad771683b6d8fcd312
Carlos Moreira <cmoreira@misfirm.com>
2024-04-29 15:28:47 -0400

    increased the OS disk size

--- main.tf
+++ main.tf
@@ -112,6 +112,7 @@ resource "azurerm_virtual_machine" "vm-rt-prod" {
   resource_group_name   = azurerm_resource_group.rg-rt-prod.name
   network_interface_ids = [azurerm_network_interface.vm-rt-prod-nic.id]
   vm_size               = "Standard_B2s"
+  os_disk_size_gb       = 64
 
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
